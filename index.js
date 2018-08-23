c93972ac8d312fa1b7b3d4c9c98b16d933679452
parisa pahlevan <paris.pahlevan@gmail.com>
2018-08-23 01:59:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,13 +3,22 @@
 const driver = {
   name: 'Sam',
   address: '11 Broadway'
+<<<<<<< HEAD
 };
 
+=======
+}
+>>>>>>> d851d599cd72ad073a7502bf0519c93ea1291b34
 function updateDriverWithKeyAndValue(driver, key, value){
   const copy = Object.assign({}, driver, {[key]: value})
   return copy
  }
+<<<<<<< HEAD
 
+=======
+ 
+ 
+>>>>>>> d851d599cd72ad073a7502bf0519c93ea1291b34
  function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
    driver[key] = value;
    return driver;
@@ -23,7 +32,20 @@ function updateDriverWithKeyAndValue(driver, key, value){
   }
   
   function destructivelyDeleteFromDriverByKey(driver, key, value){
+<<<<<<< HEAD
     delete driver[key]
     return driver;
   }
     
+=======
+   // delete driver[key]
+    return driver
+  }
+    
+    
+    
+    
+    
+    
+  }
+>>>>>>> d851d599cd72ad073a7502bf0519c93ea1291b34
